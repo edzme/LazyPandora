0b2f6f79f9ec1bac1a4f0166e72d52ef85eaff1c
ed <howtoreached@gmail.com>
2011-12-24 23:09:02 -0800

    popup shows after 1 second

--- awesome.js
+++ awesome.js
@@ -4,11 +4,47 @@ function fireEvent(element,event){
     return!element.dispatchEvent(evt);
 }
 
+var countClicks = null;
+
 function clickIfExists(){
-    var listening = document.getElementsByClassName("still_listening button btn_bg")[0];
+    var listening = document.getElementsByClassName("buttons")[0];  
+    console.log('did var listening');  
     if(listening){
         fireEvent(listening,'click');
         console.log('Booyaa');
+        countClicks++;
     }
 }
 setInterval(clickIfExists,5000);
+
+
+//showPop displays the popup.. we need to send it a variable
+function showPop(){
+    (function(d) {
+    
+    console.log('function d');
+    
+        // make the popup element
+        var popup = d.createElement('div'),
+            setStyle = function(el, styles) {
+                for(var i in styles) {
+                    el.style[i] = styles[i];
+                }
+            }
+        
+        // set the style of the popup element
+        setStyle(popup, {position: 'fixed', top: '10px', right: '10px', padding: '10px', backgroundColor: '#fff', zIndex: 1000});
+        
+        var html = [];
+            html.push('<div>',
+                        'Your music continued X times and Y videos skipped for you by Lazy Pandora Listener.',
+                        '<div style="margin-top: 10px">Buy Amazon!</div>',
+                      '</div>');
+        
+        popup.innerHTML = html.join('');
+        
+        document.body.appendChild(popup);
+    })(document);    
+}
+//displays the popup after 1000ms
+setTimeout(showPop, 1000)
